010da35b95b8c8200892e49370ac6ecf2b2c4a26
brianpkho <brianpk.ho@mail.utoronto.ca>
2020-06-07 14:07:37 -0400

    Fixed bug to not corrupt file

--- save.ps1
+++ save.ps1
@@ -1,7 +1,9 @@
 Start-Transcript -path "logs"
 try {
     $STEAM_ID = Get-ChildItem -Path "C:\Program Files (x86)\Steam\userdata" -Name
-    cd "C:\Program Files (x86)\Steam\userdata\$STEAM_ID\582010\remote\"
+    cp "C:\Program Files (x86)\Steam\userdata\$STEAM_ID\582010\remote\SAVEDATA1000" "C:\Users\Brian Ho\Documents\Github\mh_save\SAVEDATA1000"
+    cp "C:\Program Files (x86)\Steam\userdata\$STEAM_ID\582010\remote\PhotoData00.dat" "C:\Users\Brian Ho\Documents\Github\mh_save\PhotoData00.dat"
+    cd "C:\Users\Brian Ho\Documents\Github\mh_save\"
     $date = Get-Date
     git add .
     git commit -m $date
