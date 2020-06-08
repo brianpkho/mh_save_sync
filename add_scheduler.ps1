3d6e4cc61df0d7c4771770f2d4008bbdfa9e2514
brianpkho <brianpk.ho@mail.utoronto.ca>
2020-06-07 20:15:45 -0400

    updated path for mh save sync

--- add_scheduler.ps1
+++ add_scheduler.ps1
@@ -5,7 +5,7 @@ if ($(Get-ScheduledTask -TaskName "Monster Hunter Save" -ErrorAction SilentlyCon
 }
 $user = "$env:USERDOMAIN\$env:USERNAME"
 $Trigger =  New-ScheduledTaskTrigger -Daily -At "12:00pm"
-$scriptPath = '"C:\\Users\Brian Ho\\Downloads\\mh_save\\save.ps1"'
+$scriptPath = '"C:\\Users\\Brian Ho\\Documents\\Github\\mh_save_sync\\save.ps1"'
 $Principal = New-ScheduledTaskPrincipal -UserID "$env:USERDOMAIN\$env:USERNAME" -LogonType S4U -RunLevel Highest
 $action = New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-NoProfile -executionpolicy bypass -file $scriptPath -force"
 Register-ScheduledTask -TaskName "Monster Hunter Save" -Trigger $Trigger -Principal $Principal -Action $Action -Force
